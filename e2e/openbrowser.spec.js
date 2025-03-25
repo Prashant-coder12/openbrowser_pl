dd58d3a3a6a0f688ab4bd124288f808ace969234
simranpal07 <37659687+simranpal07@users.noreply.github.com>
2025-03-25 11:11:30 +0530

    Update openbrowser.spec.js

--- e2e/openbrowser.spec.js
+++ e2e/openbrowser.spec.js
@@ -1,4 +1,4 @@
-import { Text } from '@playwright/test';
+import { text } from '@playwright/test';
 test('Open browser',async ({page})=>{   
 await page.goto('https://www.demoblaze.com/');
 const pagetitle=page.title();
